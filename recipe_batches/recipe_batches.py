6a0593a6798d56b9e2ab641d376b9b6d0c2ba108
Jake Thomas <jmaxt12@gmail.com>
2019-06-06 13:41:26 -0600

    Recipe's workgit status Woot woot

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 """
 1) Understanding the Problem
+  recipe needs to have values
+  ingredients need to have values
+  if ingredients values >= recipe then you can make recipe
 2) Devise a Plan
+  the "//" in math gives me the ammount of times a number goes into another number
 3) Carry out Plan
 4) Review
 """
@@ -9,12 +13,18 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  batches = []
+  if len(recipe) == len(ingredients):
+    for key in recipe:
+      batches.append(ingredients[key]//recipe[key])
+    return min(batches)
+  else:
+      return 0
 
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  ingredients = { 'milk': 232, 'butter': 103, 'flour': 51 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
